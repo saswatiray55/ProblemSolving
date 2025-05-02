66a519184d145506d2c91030df46626e67ea41be
Saswati Ray <raysaswati195@gmail.com>
2025-05-02 12:00:15 +0530

    Minimum Step Required

---
+++ GFG/MinimumStepsRequired.cpp
@@ -0,0 +1,67 @@
+/*Minimum Steps Required
+Given a string str consisting of only two characters 'a' and 'b'. You need to find the minimum steps required to make the string empty by removing consecutive a's and b's.
+
+Example 1:
+
+Input:
+str = "bbaaabb"
+Output:
+2
+Explanation:
+Operation 1: Removal of all a's modifies str to "bbbb".
+Operation 2: Removal of all remaining b's makes str
+empty.
+Therefore, the minimum number of operations required
+is 2.
+Example 2:
+
+Input:
+str = "aababaa"
+Output:
+3
+Explanation:
+Operation 1: Removal of b's modifies str to "aaabaa".
+Operation 2: Removal of b's modifies str = "aaaaa".
+Operation 3: Removal of all remaining a's makes str 
+empty.
+Therefore, the minimum number of operations required 
+is 3.
+Your Task:
+
+You need to complete the function minSteps() which takes a string str as the only input parameter and returns an integer, denoting the minimum steps required to make the string empty.
+
+Expected Time Complexity: O(N), where N = length of string str
+Expected Space Complexity: O(1)
+
+Constraints:
+
+1 <= str.length() <= 105
+'a' <= str[i] <= 'b'*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int minSteps(string str) 
+{
+    int cnt = 1;
+ 
+    for (int i = 1; i < str.size(); i++) {
+ 
+        if (str[i] != str[i - 1]) {
+ 
+            cnt++;
+        }
+    }
+
+    return cnt / 2 + 1;
+}
+
+int main()
+{
+    string s;
+    cout << "Enter the String: ";
+    cin >> s;
+    int result = minSteps(s);
+    cout << result << " ";
+    return 0;
+}
